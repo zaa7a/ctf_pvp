b12ea1a3546ab7ca89701d48ed0f2460dfb34f13
zaa7a <zaa7a.yt.zaa7a.0213@gmail.com>
2026-05-24 13:48:02 +0900

    2

--- main.js
+++ main.js
@@ -2,28 +2,20 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 700,
+    const win = new BrowserWindow({
+        width: 1400,
+        height: 900,
         webPreferences: {
             nodeIntegration: true,
-            contextIsolated: false // 簡易化のため。本番はpreload推奨
+            contextIsolation: false
         }
     });
 
-    mainWindow.loadFile(path.join(__dirname, "client", "index.html"));
-    // 必要に応じてデベロッパーツールを開く
-    // mainWindow.webContents.openDevTools();
+    win.loadFile(path.join(__dirname, "client/index.html"));
 }
 
-app.whenReady().then(() => {
-    createWindow();
+app.whenReady().then(createWindow);
 
-    app.on("activate", function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
-});
-
-app.on("window-all-closed", function () {
+app.on("window-all-closed", () => {
     if (process.platform !== "darwin") app.quit();
 });
